a7a813165d311304cbfc9a14100354e21996bfc4
jyotiradityz <101706279+jyotiradityz@users.noreply.github.com>
2023-04-26 14:36:25 +0530

    Updated
    Small bug fixes

--- app/VotingApp.java
+++ app/VotingApp.java
@@ -4,7 +4,6 @@ import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
-// import java.io.File;
 import java.util.*;
 
 public class VotingApp extends JFrame implements ActionListener {
@@ -16,6 +15,15 @@ public class VotingApp extends JFrame implements ActionListener {
         super("Voting App");
         
         String voterid = JOptionPane.showInputDialog(this, "Enter Voter ID:");
+        System.out.println(voterid);
+        if(voterid.equals("")){
+            int result = JOptionPane.showConfirmDialog(null, "Do you want to view the result?", "View Result", JOptionPane.YES_NO_OPTION);
+  
+            // if user selects yes, call the result method
+            if (result == JOptionPane.YES_OPTION) {
+                result();
+            }
+        }
 
         Boolean hasnotvoted = true;
         try{
@@ -38,11 +46,13 @@ public class VotingApp extends JFrame implements ActionListener {
 
         }
         catch(Exception e){
-            System.out.println(e.getMessage());
+             
         }
 
         if(hasnotvoted){
             candidateLabel = new JLabel("Select Candidate:");
+            candidateLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
             candidateComboBox = new JComboBox<String>();
 
             candidateComboBox.addItem("Viraj");
@@ -51,6 +61,9 @@ public class VotingApp extends JFrame implements ActionListener {
 
             voteButton = new JButton("Vote");
             voteButton.addActionListener(this);
+            voteButton.setBackground(Color.GREEN);
+            voteButton.setText("Cast Vote");
+            voteButton.setHorizontalAlignment(SwingConstants.CENTER);
 
             // Set layout and add components
             setLayout(new FlowLayout());
@@ -58,48 +71,56 @@ public class VotingApp extends JFrame implements ActionListener {
             add(candidateComboBox);
             add(voteButton);
 
-            // Connect to database
-            try {
-                
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(this, "Database error: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-                System.exit(1);
-            }
-
             // Set window properties
-            setSize(300, 100);
+            setSize(800, 400);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             setLocationRelativeTo(null);
             setVisible(true);
         }
         else{
-            System.out.println("the user has already casted vote");
+            for (Window window : Window.getWindows()) {
+                window.dispose();
+            }
+
+            JOptionPane.showMessageDialog(this, "You have already voted.", "Error", JOptionPane.ERROR_MESSAGE);
+            JButton okButton = new JButton("OK");
+            okButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    for (Window window : Window.getWindows()) {
+                        window.dispose();
+                    }
+                }
+            });
+            add(okButton);
+            setSize(400, 150);
+            
+
+            new VotingApp();
         }
-        
     }
 
     public void actionPerformed(ActionEvent e) {
         // Get selected candidate name
         String candidateName = (String) candidateComboBox.getSelectedItem();
-
+    
         try {
-
+    
             BufferedReader br = new BufferedReader(new FileReader("data.txt")) ;
-
+    
             ArrayList<String> list = new ArrayList<>() ;
-
+    
             String line ;
             while((line = br.readLine()) != null){
                 list.add(line) ;
             }
-
+    
             FileWriter file = new FileWriter("data.txt") ;
             
-
+    
             boolean foundCandidate = false ;
             for(String str: list){
                 String[] arr = str.split(" ") ;
-
+    
                 if(arr[0].equals(candidateName)){
                     int votes = Integer.parseInt(arr[1]) + 1 ;
                     arr[1] = Integer.toString(votes) ;
@@ -108,20 +129,32 @@ public class VotingApp extends JFrame implements ActionListener {
                 String newStr = arr[0] +" " + arr[1] ;
                 file.write(newStr+"\n");
             }
-
+    
             if(!foundCandidate){
                 file.write(candidateName +" 1\n");
             }
-
+    
             file.flush();
             file.close();
             br.close();
-
-
+    
+    
             // Show success message
             JOptionPane.showMessageDialog(this, "Vote cast for " + candidateName, "Success",JOptionPane.INFORMATION_MESSAGE);
+    
+            // Display candidate information
+            JLabel candidateInfoLabel = new JLabel();
+            if(candidateName.equals("Viraj")) {
+                candidateInfoLabel.setText("Party : XYZ");
+            }
+            else if(candidateName.equals("Jyot")) {
+                candidateInfoLabel.setText("party : ABC");
+            }
+            candidateInfoLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            add(candidateInfoLabel);
+    
         } catch (Exception ex) {
-            JOptionPane.showMessageDialog(this, "Database error: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+             
         }
 
         for (Window window : Window.getWindows()) {
@@ -129,6 +162,47 @@ public class VotingApp extends JFrame implements ActionListener {
         }
 
         new VotingApp();
+
+    }
+
+    public static void result(){
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("data.txt")) ;
+            ArrayList<String> list = new ArrayList<>() ;
+            
+            String line;
+
+            while((line = br.readLine()) != null){
+                list.add(line);
+            }
+            int Viraj=0,Jyot=0;
+            for(String str: list){
+                String[] arr = str.split(" ") ;
+                if(arr[0].equals("Viraj")){
+                    Viraj=Integer.parseInt(arr[1]);
+                }
+                else{
+                    Jyot = Integer.parseInt(arr[1]);
+                }
+            }
+
+            String result;
+            if (Viraj > Jyot) {
+                result = "Viraj won by " + (Viraj - Jyot) + " Votes";
+            } else if (Viraj < Jyot) {
+                result = "Jyot won by " + Math.abs(Viraj - Jyot) + " Votes";
+            } else {
+                result = "It's a Draw! Stay tuned for next Elections...";
+            }
+
+            JOptionPane.showMessageDialog(null, result, "Election Result", JOptionPane.INFORMATION_MESSAGE);
+
+            br.close();
+            
+        }
+        catch(Exception e){
+             
+        }
     }
 
     public static void main(String[] args) {
